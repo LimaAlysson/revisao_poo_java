f01fb9bf8ef3f629aa2835fa7fbc2ee96d1a99b3
LimaAlysson <alyssonredes32@gmail.com>
2025-03-17 19:48:04 -0300

    pequena revisao de poo usando java

---
+++ teste/Livro.java
@@ -0,0 +1,25 @@
+package teste;
+
+public class Livro {
+	private String titulo;
+	private int publicacao;
+	
+	public Livro() {
+//		construtor vazio
+	}
+	
+	
+	public int getPublicacao() {
+		return publicacao;
+	}
+	public void setPublicacao(int publicacao) {
+		this.publicacao = publicacao;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	
+}
---
+++ teste/Main.java
@@ -0,0 +1,10 @@
+package teste;
+
+public class Main {
+	public static void main(String[] args) {
+		Livro livro = new Livro();
+		livro.setTitulo("POO 2");
+		
+		System.out.println(livro.getTitulo());
+	}
+}
