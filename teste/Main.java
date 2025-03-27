b2e572604b40ae9ce31a8451f909d16db4b1c8bd
LimaAlysson <alyssonredes32@gmail.com>
2025-03-26 21:03:08 -0300

    trabalhando com herança

--- teste/Main.java
+++ teste/Main.java
@@ -1,10 +1,16 @@
 package teste;
 
+import javax.swing.JOptionPane;
+
 public class Main {
 	public static void main(String[] args) {
 		Livro livro = new Livro();
 		livro.setTitulo("POO 2");
+		int publicacao = Integer.parseInt(JOptionPane.showInputDialog("Qual o ano de publicação?"));
+		
+		livro.setPublicacao(publicacao);
 		
 		System.out.println(livro.getTitulo());
+		JOptionPane.showMessageDialog(null, livro.getTitulo()+" \n"+livro.getPublicacao());
 	}
 }
